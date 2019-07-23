40057694f4d32001773e95edabd655a271c599ee
Noman Ullah <nomi.spyko@gmail.com>
2019-07-24 01:28:23 +0500

    delete

--- scripts/index.js
+++ scripts/index.js
@@ -178,6 +178,23 @@ function scrollFunction(){
   }
 }
 
+// Tpewrite
+window.onload = function() {
+    var elements = document.getElementsByClassName('typewrite');
+    for (var i=0; i<elements.length; i++) {
+        var toRotate = elements[i].getAttribute('data-type');
+        var period = elements[i].getAttribute('data-period');
+        if (toRotate) {
+          new TxtType(elements[i], JSON.parse(toRotate), period);
+        }
+    }
+    // INJECT CSS
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+    document.body.appendChild(css);
+};
+
 // Resize Functions
 function resizeFunc(){
   
@@ -194,3 +211,4 @@ $(document).ready(function(){
 $(window).resize(function(){
   resizeFunc();
 });
+
